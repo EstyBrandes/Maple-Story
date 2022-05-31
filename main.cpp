cc7b97e96049bae0b3e4d5b975fdfcacff980955
Reshaflame <102427924+Reshaflame@users.noreply.github.com>
2022-05-31 17:03:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,30 @@
 
 using namespace std;
 
-User globalUser("YuviVerse", "TwitchTTV");
+// making global characters and enemies and user to check modules:
+
+// Enemies:
+Enemy monster1 = Enemy("Goblin", 5, 5);
+Enemy monster2 = Enemy("Wolf");
+Enemy monster3 = Enemy("Soldier");
+Enemy monster4 = Enemy("Ogre", 20);
+Enemy monster5 = Enemy("Dragon", 30, 12);
+Enemy* monsters = new Enemy[5] {monster1,monster2, monster3, monster4, monster5}; 
+
+// Characters:
+Character hero1 = Character("Gojo",Magician);
+Character hero2 = Character("Itadori",Warrior);
+Character hero3 = Character("Fushiguro",Pirate);
+Character hero4 = Character("Kugisaki",Rouge, 40);
+Character hero5 = Character("Yuta", Archer, 60, 30);
+Character* heroes = new Character[5]{hero1, hero2, hero3, hero4, hero5};
+
+// User:
+User globalUser("YuviVerse", "TwitchTTV",heroes);
 
 int main()
 {
-    Enemy monster1 = Enemy("Goblin", 5, 5);
-    Enemy monster2 = Enemy("Wolf");
-    Enemy monster3 = Enemy("Soldier");
-    Enemy monster4 = Enemy("Ogre", 20);
-    Enemy monster5 = Enemy("Dragon", 30, 12);
-    Enemy* monsters = new Enemy[5] {monster1,monster2, monster3, monster4, monster5}; 
-    Character hero1 = Character("Gojo",Magician);
-    Character hero2 = Character("Itadori",Warrior);
-    Character hero3 = Character("Fushiguro",Pirate);
-    Character hero4 = Character("Kugisaki",Rouge, 40);
-    Character hero5 = Character("Yuta", Archer, 60, 30);
-    Character* heroes = new Character[5]{hero1, hero2, hero3, hero4, hero5};
+    
     char logName[10];
     char logPass[10];
     cout << " \tXX Please Enter Your User Name: XX\n";
@@ -43,7 +51,6 @@ int main()
 /*
     ~ get enemy into array [Almog: "Done!"]
     ~ get characters into array [Almog: "Done!"]
-    ~ putting the arrays of enemys and characters to use
     ~ Level up: next level is X2 the expirience capacity, Attack power & Health points is X1.5
     ~ manage exeptions in all constractors
     ~ manage exceptions if all functions and methods
